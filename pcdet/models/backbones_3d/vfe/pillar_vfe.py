05de385baddc232884aaa1393b4e0d63e722c1e8
ShengStar <20211401015@cqu.edu.cn>
2022-10-14 15:10:30 +0800

    添加点分类，可以正常运行

--- pcdet/models/backbones_3d/vfe/pillar_vfe.py
+++ pcdet/models/backbones_3d/vfe/pillar_vfe.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from ....utils import box_utils, common_utils
+from ....ops.roiaware_pool3d import roiaware_pool3d_utils
 
 from .vfe_template import VFETemplate
 
@@ -151,6 +153,123 @@ class PillarVFE(VFETemplate):
         paddings_indicator = actual_num.int() > max_num
         return paddings_indicator
 
+    def assign_targets(self, input_dict):
+        """
+        Args:
+            input_dict:
+                point_features: (N1 + N2 + N3 + ..., C)
+                batch_size:
+                point_coords: (N1 + N2 + N3 + ..., 4) [bs_idx, x, y, z]
+                gt_boxes (optional): (B, M, 8)
+        Returns:
+            point_cls_labels: (N1 + N2 + N3 + ...), long type, 0:background, -1:ignored
+            point_part_labels: (N1 + N2 + N3 + ..., 3)
+        """
+        
+        point_coords = input_dict['point_coords'] #torch.Size([4096, 4])
+        gt_boxes = input_dict['gt_boxes'] # torch.Size([2, 31, 8])
+        # [0.2, 0.2, 0.2]
+        assert gt_boxes.shape.__len__() == 3, 'gt_boxes.shape=%s' % str(gt_boxes.shape)
+        assert point_coords.shape.__len__() in [2], 'points.shape=%s' % str(point_coords.shape)
+
+        batch_size = gt_boxes.shape[0]
+        extend_gt_boxes = box_utils.enlarge_box3d(
+            gt_boxes.view(-1, gt_boxes.shape[-1]), extra_width=[0.2, 0.2, 0.2]
+        ).view(batch_size, -1, gt_boxes.shape[-1])
+        targets_dict = self.assign_stack_targets(
+            points=point_coords, gt_boxes=gt_boxes, extend_gt_boxes=extend_gt_boxes,
+            set_ignore_flag=True, use_ball_constraint=False,
+            ret_part_labels=False
+        )
+        return targets_dict
+
+    # 点分类
+
+    def assign_stack_targets(self, points, gt_boxes, extend_gt_boxes=None,
+                             ret_box_labels=False, ret_part_labels=False,
+                             set_ignore_flag=True, use_ball_constraint=False, central_radius=2.0):
+        """
+        Args:
+            points: (N1 + N2 + N3 + ..., 4) [bs_idx, x, y, z]
+            gt_boxes: (B, M, 8)
+            extend_gt_boxes: [B, M, 8]
+            ret_box_labels:
+            ret_part_labels:
+            set_ignore_flag:
+            use_ball_constraint:
+            central_radius:
+
+        Returns:
+            point_cls_labels: (N1 + N2 + N3 + ...), long type, 0:background, -1:ignored
+            point_box_labels: (N1 + N2 + N3 + ..., code_size)
+
+        """
+        assert len(points.shape) == 2 and points.shape[1] == 4, 'points.shape=%s' % str(points.shape)
+        assert len(gt_boxes.shape) == 3 and gt_boxes.shape[2] == 8, 'gt_boxes.shape=%s' % str(gt_boxes.shape)
+        assert extend_gt_boxes is None or len(extend_gt_boxes.shape) == 3 and extend_gt_boxes.shape[2] == 8, \
+            'extend_gt_boxes.shape=%s' % str(extend_gt_boxes.shape)
+        assert set_ignore_flag != use_ball_constraint, 'Choose one only!'
+        batch_size = gt_boxes.shape[0]
+
+        bs_idx = points[:, 0]
+
+        point_cls_labels = points.new_zeros(points.shape[0]).long()
+        point_box_labels = gt_boxes.new_zeros((points.shape[0], 8)) if ret_box_labels else None
+        point_part_labels = gt_boxes.new_zeros((points.shape[0], 3)) if ret_part_labels else None
+        for k in range(batch_size):
+            bs_mask = (bs_idx == k)
+            points_single = points[bs_mask][:, 1:4]
+            point_cls_labels_single = point_cls_labels.new_zeros(bs_mask.sum())
+            box_idxs_of_pts = roiaware_pool3d_utils.points_in_boxes_gpu(
+                points_single.unsqueeze(dim=0), gt_boxes[k:k + 1, :, 0:7].contiguous()
+            ).long().squeeze(dim=0)
+            box_fg_flag = (box_idxs_of_pts >= 0)
+            if set_ignore_flag:
+                extend_box_idxs_of_pts = roiaware_pool3d_utils.points_in_boxes_gpu(
+                    points_single.unsqueeze(dim=0), extend_gt_boxes[k:k+1, :, 0:7].contiguous()
+                ).long().squeeze(dim=0)
+                fg_flag = box_fg_flag
+                ignore_flag = fg_flag ^ (extend_box_idxs_of_pts >= 0)
+                point_cls_labels_single[ignore_flag] = -1
+            elif use_ball_constraint:
+                box_centers = gt_boxes[k][box_idxs_of_pts][:, 0:3].clone()
+                box_centers[:, 2] += gt_boxes[k][box_idxs_of_pts][:, 5] / 2
+                ball_flag = ((box_centers - points_single).norm(dim=1) < central_radius)
+                fg_flag = box_fg_flag & ball_flag
+            else:
+                raise NotImplementedError
+
+            gt_box_of_fg_points = gt_boxes[k][box_idxs_of_pts[fg_flag]]
+            self.num_class = 1
+            point_cls_labels_single[fg_flag] = 1 if self.num_class == 1 else gt_box_of_fg_points[:, -1].long()
+            point_cls_labels[bs_mask] = point_cls_labels_single
+
+            if ret_box_labels and gt_box_of_fg_points.shape[0] > 0:
+                point_box_labels_single = point_box_labels.new_zeros((bs_mask.sum(), 8))
+                fg_point_box_labels = self.box_coder.encode_torch(
+                    gt_boxes=gt_box_of_fg_points[:, :-1], points=points_single[fg_flag],
+                    gt_classes=gt_box_of_fg_points[:, -1].long()
+                )
+                point_box_labels_single[fg_flag] = fg_point_box_labels
+                point_box_labels[bs_mask] = point_box_labels_single
+
+            if ret_part_labels:
+                point_part_labels_single = point_part_labels.new_zeros((bs_mask.sum(), 3))
+                transformed_points = points_single[fg_flag] - gt_box_of_fg_points[:, 0:3]
+                transformed_points = common_utils.rotate_points_along_z(
+                    transformed_points.view(-1, 1, 3), -gt_box_of_fg_points[:, 6]
+                ).view(-1, 3)
+                offset = torch.tensor([0.5, 0.5, 0.5]).view(1, 3).type_as(transformed_points)
+                point_part_labels_single[fg_flag] = (transformed_points / gt_box_of_fg_points[:, 3:6]) + offset
+                point_part_labels[bs_mask] = point_part_labels_single
+
+        targets_dict = {
+            'point_cls_labels': point_cls_labels,
+            'point_box_labels': point_box_labels,
+            'point_part_labels': point_part_labels
+        }
+        return targets_dict
+
     def forward(self, batch_dict, **kwargs):
         points = batch_dict['points']
         batch_size = batch_dict['batch_size']
@@ -161,11 +280,12 @@ class PillarVFE(VFETemplate):
         batch_num_points = []
         for i in range(batch_size):
             mask = points[:,0] == i
+            gt_box_cls = gt_box[i,:].unsqueeze(0)
             point_s = points[mask]
-            # gt_box_cls = gt_box[i]
-            # print(gt_box_cls.shape)
-
             points_cls = point_s[:,0:4]
+            points_cls[:,0] = 0
+            dict_cls = {'gt_boxes':gt_box_cls,'point_coords':points_cls}
+            targets_dict = self.assign_targets(dict_cls)
 
             voxel_output = self.voxel_generator.generate(point_s[:,1:5].cpu().numpy())
             voxels, coordinates, num_points = voxel_output
